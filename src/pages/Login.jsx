17c4313384bd36e0df7ff672c3b96ec52b884b22
vncntkyl <kyle.rinoza2009@gmail.com>
2023-06-17 18:50:53 +0800

    initial setup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,15 @@
-import React from "react";
 import login_img from "../assets/login_img.jpg";
 
 export default function Login() {
   return (
     <div className="w-screen h-screen flex justify-center items-center p-2">
-      <div className="flex flex-col w-full rounded-md overflow-hidden">
-        <div className="bg-login bg-cover bg-center min-h-[100px] lg:min-h-[200px] w-full">
-          {/* <img
+      <div className="flex flex-col lg:flex-row w-full rounded-md overflow-hidden">
+        <div className="bg-login bg-cover bg-center min-h-[100px] sm:min-h-[200px] lg:bg-none w-full">
+          <img
             src={login_img}
             alt="Edwin And Lina Poultry Farm"
             className="w-full"
-          /> */}
+          />
         </div>
         <div className="bg-main p-2">
           <p>Edwin and Lina Poultry Farm</p>
