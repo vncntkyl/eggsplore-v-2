650ce553247b8b27bd0c2e4e0aa2c7badbb4bfff
vncntkyl <kyle.rinoza2009@gmail.com>
2023-06-17 21:37:05 +0800

    edited config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,45 @@ export default {
       backgroundImage: {
         login: "url('./assets/login_img.jpg')",
       },
+      transitionTimingFunction: {
+        toggle: "cubic-bezier(0,.72,1,.52)",
+      },
+      animation: {
+        "slide-down": "slide-down 500ms ease-in-out forwards",
+        "slide-right": "slide-right 200ms ease-in-out forwards",
+        fade: "fade 200ms ease-in-out forwards",
+      },
+      keyframes: {
+        "slide-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-100%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "slide-right": {
+          "0%": {
+            transform: "translateX(-100%)",
+          },
+          "100%": {
+            transform: "translateX(0)",
+          },
+        },
+        fade: {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      },
       colors: {
         main: "#B95446",
+        secondary: "#f46b45",
       },
     },
   },
