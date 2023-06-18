5bbf7e20e389014c80e77dd95d1bdf1b0f128495
vncntkyl <kyle.rinoza2009@gmail.com>
2023-06-19 06:32:16 +0800

    minor css changes

--- src/components/Containers/Alert.jsx
+++ src/components/Containers/Alert.jsx
@@ -14,9 +14,7 @@ export default function Alert({ type = "info", title, message, onClose }) {
         className="fixed top-0 w-screen h-screen flex items-center justify-center bg-[#00000035] z-[21]"
         onClick={onClose}
       ></div>
-      <div
-        className="absolute rounded-md shadow-sm w-fit overflow-hidden z-[22] animate-slide-down"
-      >
+      <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] rounded-md shadow-sm w-fit overflow-hidden z-[22] animate-modal-slide-down">
         <div
           className={classNames(
             "p-2 flex items-center gap-2",
